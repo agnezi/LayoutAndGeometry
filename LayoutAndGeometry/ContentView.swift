5c3eb47fb8684308376ccacc10af3fe4d4cbd7d3
Jonas Agnezi <jonas.agnezi@gmail.com>
2023-02-26 15:21:17 -0300

    Initial Commit

---
+++ LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,25 @@
+////
+// LayoutAndGeometry
+// Created by: itsjagnezi on 26/02/23
+// Copyright (c) today and beyond
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,16 @@
+////
+// LayoutAndGeometry
+// Created by: itsjagnezi on 26/02/23
+// Copyright (c) today and beyond
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
